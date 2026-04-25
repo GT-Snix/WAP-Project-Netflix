bf7e2a90279d96a72a9b604d79f4b2c621c3eccf
Rohan Prathap Reddy V <rohanprathapreddy@Macbook-Pro-10.local>
2026-04-25 20:12:44 +0530

    Hero Page adjustments

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,6 @@
+import {useState, useEffect} from 'react'
 import useFetch from './hooks/useFetch';
+import MovieModal from './MovieModal';
 import './Hero.css';
 
 const IMG_BASE = 'https://image.tmdb.org/t/p/original';
@@ -6,7 +8,19 @@ const API_KEY = import.meta.env.VITE_TMDB_KEY;
 const TRENDING_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&language=en-US`;
 
 function Hero() {
-  console.log(TRENDING_URL)
+  const [mov, setMov] = useState(0);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setMov((prev) => {
+        return (prev + 1) % 3
+      });
+    }, 5000);
+
+    return ()=>clearInterval(interval);
+  }, []);
+  // console.log(TRENDING_URL)
   const { data, loading, error } = useFetch(TRENDING_URL);
 
   if (loading) {
@@ -21,7 +35,7 @@ function Hero() {
     );
   }
 
-  const movie = data.results[0];
+  const movie = data.results[mov];
   const title = movie.title || movie.name || 'Untitled';
   const backdropUrl = movie.backdrop_path
     ? `${IMG_BASE}${movie.backdrop_path}`
@@ -34,13 +48,13 @@ function Hero() {
     console.log(movie.title)
 
   return (
-    <section className="banner" aria-label={`Featured movie: ${title}`}>
+    <section className="banner">
       {backdropUrl && (
         <div
           className="banner-bg"
           style={{ backgroundImage: `url(${backdropUrl})` }}
           role="img"
-          aria-label={`${title} backdrop`}
+          loading="lazy"
         />
       )}
 
@@ -49,15 +63,19 @@ function Hero() {
         <p className="banner-text">{shortOverview}</p>
 
         <div className="banner-actions">
-          <button className="btn btn-play" id="hero-play-btn">
-            <span aria-hidden="true">▶</span> Play
+          <button className="btn btn-play" id="hero-play-btn" onClick={() => setSelectedMovie(movie)}>
+            <span>▶</span> Play
           </button>
 
-          <button className="btn btn-info" id="hero-info-btn">
-            <span aria-hidden="true">ⓘ</span> More Info
+          <button className="btn btn-info" id="hero-info-btn" onClick={() => setSelectedMovie(movie)}>
+            <span>ⓘ</span> More Info
           </button>
         </div>
       </div>
+      <MovieModal
+        movie={selectedMovie}
+        onClose={() => setSelectedMovie(null)}
+      />
     </section>
   );
 }
